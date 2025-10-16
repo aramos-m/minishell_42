d9d9af6c1758dcabc121fc31feafaf53a33490f9
aramos-m <anaana609@gmail.com>
2025-10-16 20:29:02 +0200

    feat: add built-in echo -n (refs #5)

--- executor.c
+++ executor.c
@@ -3,6 +3,33 @@
 #include <readline/readline.h> //readline
 #include <readline/history.h> // add_history
 #include <unistd.h> // execve, fork
+#include <string.h> // strncmp
+
+// Built in: echo -n
+void    echo(char **args)
+{
+    int arg_n;
+    int i;
+
+    i = 1;
+    if(args[1][0] == '-' && args[1][1] == 'n' && args[1][2]  == '\0')
+    {
+        arg_n = 1;
+        i++;
+    }
+    while(args[i])
+    {
+        printf("%s", args[i]);
+        i++;
+        if(args[i] != NULL)
+            printf(" ");
+    }
+    if(arg_n != 1)
+    {
+        printf("\n");
+    }
+    return ;
+}
 
 char	**ft_split(char const *s, char c);
 
@@ -27,13 +54,16 @@ int main(void)
             add_history(input);
             args = ft_split(input, ' ');
 
+            if(strncmp(args[0], "echo", 5) == 0) // Para que busque también el final de la string
+                echo(args);
+
             if(access(args[0], X_OK) == 0)
             {
                 pid = fork();
                 if (pid == 0)
                     execve(args[0], args, NULL); // args: const char *path, char *const _Nullable argv[], char *const _Nullable envp[]
                 else
-                    wait(); // Espera a el hijo
+                    wait(); // Espera al hijo
             }
         }
         free(input);
